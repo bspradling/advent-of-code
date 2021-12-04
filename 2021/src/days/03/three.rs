43b4f44cb23657a5997a6e6ed4498a64daf2a9fa
Brett Spradling <brett@spradling.me>
2021-12-04 08:30:18 -0800

    small clean up

--- 2021/src/days/03/three.rs
+++ 2021/src/days/03/three.rs
@@ -15,14 +15,13 @@ pub fn compute_rates(report: Vec<&str>) -> Result<(String, String)> {
 
     let (gamma, epsilon) = (0..entry_length)
         .map(|i| determine_common_bit_for_column(&report, i, '1').unwrap())
-        .map(|bit| match bit {
-            '1' => (1, 0),
-            '0' => (0, 1),
-            _ => panic!("foooo"),
-        })
         .fold(
             (String::new(), String::new()),
-            |(gamma, epsilon), (x, y)| (format!("{}{}", gamma, x), format!("{}{}", epsilon, y)),
+            |(gamma, epsilon), x| match x {
+                '1' => (format!("{}{}", gamma, '1'), format!("{}{}", epsilon, '0')),
+                '0' => (format!("{}{}", gamma, '0'), format!("{}{}", epsilon, '1')),
+                _ => panic!("foo"),
+            },
         );
 
     Ok((gamma, epsilon))
@@ -50,15 +49,15 @@ pub fn determine_common_bit_for_column(
 }
 
 pub fn part_two(report: Vec<&str>) -> Result<u32> {
-    let (gamma, epsilon) = compute_rates_2(report)?;
-    let gamma_value = u32::from_str_radix(gamma.as_str(), 2)?;
-    let epsilon_value = u32::from_str_radix(epsilon.as_str(), 2)?;
-    Ok(gamma_value * epsilon_value)
+    let (oxygen, co2) = compute_rates_2(report)?;
+    let oxygen_value = u32::from_str_radix(oxygen.as_str(), 2)?;
+    let co2_value = u32::from_str_radix(co2.as_str(), 2)?;
+    Ok(oxygen_value * co2_value)
 }
 
 pub fn compute_rates_2(report: Vec<&str>) -> Result<(String, String)> {
-    let oxygen = filter_ratings(report.clone(), 0, |x, y| x.eq(&y), '1')?;
-    let co2 = filter_ratings(report.clone(), 0, |x, y| x.ne(&y), '1')?;
+    let oxygen = filter_ratings(report.clone(), 0, |x, y| x.eq(&y))?;
+    let co2 = filter_ratings(report.clone(), 0, |x, y| x.ne(&y))?;
 
     Ok((oxygen, co2))
 }
@@ -67,7 +66,6 @@ fn filter_ratings(
     values: Vec<&str>,
     index: usize,
     filter_condition: fn(char, char) -> bool,
-    tie: char,
 ) -> Result<String> {
     if values.len() == 1 {
         return Ok(values[0].to_string());
@@ -83,6 +81,5 @@ fn filter_ratings(
             .collect::<Vec<&str>>(),
         index + 1,
         filter_condition,
-        tie,
     )
 }
--- 2021/src/days/mod.rs
+++
@@ -1,6 +0,0 @@
-#[path = "01/one.rs"]
-pub mod one;
-#[path = "03/three.rs"]
-pub mod three;
-#[path = "02/two.rs"]
-pub mod two;
